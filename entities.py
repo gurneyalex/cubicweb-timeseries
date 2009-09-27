d53dd66ca65b5017d45ebcedb5d3e58e84a43d0d
Alexandre Fayolle <alexandre.fayolle@logilab.fr>
2009-09-27 21:20:25 +0200

    added calendar management classes
    * Normalized Calendar is functional
    * Gregorian Calendar is functional
    * Gas Calendar : to be done

--- entities.py
+++ entities.py
@@ -98,6 +98,10 @@ class TimeSeries(AnyEntity):
 
 
 
+    @property
+    def calendar(self):
+        return ALL_CALENDARS[self.use_calendar]
+
     def _numpy_from_csv(self, file):
         sniffer = csv.Sniffer()
         raw_data = file.read()
@@ -149,3 +153,185 @@ class TimeSeries(AnyEntity):
                          
             
 
+class AbstractCalendar:
+    
+    def get_offset(self, date, granularity):
+        if isinstance(date, str):
+            date = self._parse_iso(date)
+        offset_method = getattr(self, '_get_offset_%s'%granularity)
+        return offset_method(date)
+
+    def _get_offset_15min(self, date):
+        return (date._ordinal*24+date.hour)*4 + date.minute//15
+
+    def _get_offset_1h(self, date):
+        return date._ordinal*24+date.hour # XXX DST!
+
+    def _get_offset_1d(self, date):
+        return date._ordinal
+
+    def _get_offset_1w(self, date):
+        return date._ordinal//7
+
+    def _get_offset_1m(self, date):
+        return (date.year-1)*12+date.month-1
+
+    def _get_offset_1y(self, date):
+        return date.year-1
+
+    def _parse_iso(self, isodate):
+        if len(isodate) == 8: # just a date
+            date = isodate
+            hour = 0
+            minute = 0
+            second = 0
+        else:
+            assert isodate[8] == 'T'
+            date, time = isodate.split('T')
+            hour, minute, sec = time.split(':')
+            hour = int(hour)
+            minute = int(minute)
+            second = int(floor(float(sec)))
+        year = int(date[:4])
+        month = int(date[4:6])
+        day = int(date[6:])
+
+        return DateTime(year, month, day, hour, minute, second, self, isodate)
+
+    def date_from_ordinal_and_seconds(self, ordinal, seconds=0):
+        """
+        return a DateTime instance with the given ordinal ans seconds
+        """
+        year, day, month = self._ymd_from_ordinal(ordinal)
+        hour, minute, second = self._hms_from_seconds(seconds)
+        return DateTime(year, month, day, hour, minute, second, self)
+
+    def _ymd_from_ordinal(self, ordinal):
+        raise NotImplementedError
+    
+    def _hms_from_seconds(self, seconds):
+        hour, rem_secs = divmod(seconds, 3600)
+        minute, second = divmod(rem_secs, 60)
+        return hour, minute, second
+
+    def ordinal(self, date):
+        """
+        return the number of days since Jan 1st, 0001 (this one being having ordinal 0)
+        """
+        raise NotImplementedError
+
+    def seconds(self, date):
+        """
+        return the number of seconds since the begining of the day for that date
+        """
+        return date.second+60*date.minute+3600*date.hour # XXX DST
+
+    def day_of_week(self, date):
+        """
+        return the day of week for a given date as an integer (0 is monday -> 6 is sunday)
+        """
+        raise NotImplementedError
+                  
+class DateTime:
+    """
+    representation of a date + time, linked to a calendar.
+    Meant to be immutable. Please consider the attributes read-only.
+
+    XXX timezone management (core work required in calendar)
+    
+    """
+    def __init__(self, year, month, day, hour, minute, second, calendar, iso_str=None):
+        # boundary checks
+        assert 0 < year < 3000
+        assert 1 <= month <= 12
+        assert 1 <= day <= 31 # could be enhanced
+        assert 0 <= hour < 24
+        assert 0 <= minute < 60
+        assert 0 <= second < 60
+        
+        self._calendar = calendar
+        if iso_str is None:
+            iso_str = '%d%d%dT%d:%d:%d' % (year, month, day, hour, minute, second)
+        self._iso_str = iso_str
+        self.year = year
+        self.month = month
+        self.day = day
+        self.hour = hour
+        self.minute = minute
+        self.second = second
+        # self._ordinal : integer giving the number of days since the epoch
+        self._ordinal = calendar.ordinal(self)
+        # self._seconds : integer giving the number of seconds since the beginning of day
+        self._seconds = calendar.seconds(self)
+
+    def __repr__(self):
+        return "<DateTime %s>" % self._iso_str
+
+    def as_iso(self):
+        return self._iso_str
+
+    def as_datetime(self):
+        return datetime.datetime(self.year, self.month, self.day, self.hour, self.minute, self.second)
+
+    def day_of_week(self):
+        return self._calendar.day_of_week(self)
+
+    def __eq__(self, other):
+        if not isinstance(other, DateTime):
+            return NotImplemented
+        # XXX can we safely use identity of calendars ?
+        return self._calendar is other._calendar and \
+               self._ordinal == other._ordinal and \
+               self._seconds == other._seconds
+
+
+
+    
+
+class GregorianCalendar(AbstractCalendar):
+    def ordinal(self, date):        
+        return self.__as_datetime(date).toordinal()
+
+    @staticmethod
+    def __as_datetime(date):
+        return datetime.datetime(date.year, date.month, date.day,
+                                 date.hour, date.minute, date.second)
+
+    def day_of_week(self, date):
+        return self.__as_datetime(date).weekday()
+
+    def _ymd_from_ordinal(self, ordinal):
+        date = datetime.fromordinal(ordinal)
+        return date.year, date.month, date.day
+        
+class GasCalendar(AbstractCalendar):
+    pass
+
+class NormalizedCalendar(AbstractCalendar):
+    """
+    Normalized calendar has 365 days, and starts on monday
+    XXX: DST ? 
+    """
+    def __init__(self):
+        self.month_length = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+        self.cum_month_length =  [0] + numpy.cumsum(self.month_length[:-1]).tolist()
+        
+    def ordinal(self, date):
+        return (date.year-1)*365 + self.cum_month_length[date.month-1] + date.day-1 
+
+    def day_of_week(self, date):
+        return (self.cum_month_length[date.month-1] + date.day-1) % 7
+
+    def _ymd_from_ordinal(self, ordinal):
+        year, days = divmod(ordinal, 365)
+        year = year+1
+        for month, cumdays in enumerate(self.cum_month_length):
+            if days < cumdays:
+                break
+        day = days - self.cum_month_length[month-1] + 1
+        return year, day, month
+
+ALL_CALENDARS = {'gregorian': GregorianCalendar(),
+                 'normalized': NormalizedCalendar(),
+                 'gas': GasCalendar(),
+                 }
