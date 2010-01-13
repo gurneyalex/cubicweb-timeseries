4b3da17d4e107adf4404443b03090505811e21e6
David Laudy <David Laudy@localhost>
2010-01-13 10:30:51 +0100

    create special javascripts for TS plots

---
+++ data/timeseries.flot.js
@@ -0,0 +1,33 @@
+function showTooltip(x, y, contents) {
+    $('<div id="tooltip">' + contents + '</div>').css( {
+            position: 'absolute',
+        display: 'none',
+        top: y + 5,
+            left: x + 5,
+            border: '1px solid #fdd',
+            padding: '2px',
+            'background-color': '#fee',
+            opacity: 0.80
+        }).appendTo("body").fadeIn(200);
+}
+
+var previousPoint = null;
+function onTSPlotHover(event, pos, item) {
+    if (item) {
+        if (previousPoint != item.datapoint) {
+            previousPoint = item.datapoint;
+            $("#tooltip").remove();
+            var x = item.datapoint[0].toFixed(2),
+                y = item.datapoint[1].toFixed(2);
+            
+            x = new Date(item.datapoint[0]);
+            x = x.strftime("%Y/%m/%d %H:%M");
+            
+            showTooltip(item.pageX, item.pageY,
+            item.series.label + ': (' + x + ' ; ' + y + ')');
+        }
+    } else {
+        $("#tooltip").remove();
+        previousPoint = null;
+    }
+}
