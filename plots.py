13c2deff7a5a0880b510b709a1a16ede7aadb052
patrick watteyne <patrick.watteyne@gdfsuez.com>
2010-02-17 12:08:00 +0100

    New plot renderer

--- plots.py
+++ plots.py
@@ -116,3 +116,125 @@ if ((mainfig.attr('cubicweb:type') != 'prepared-plot') || (overviewfig.attr('cub
                                      },
                                     jsoncall=req.form.get('jsoncall', False))
 
+class TSHighChartsPlotWidget(PlotWidget):
+    """PlotRenderer widget using HighCharts"""
+    onload = u"""
+var mainfig = jQuery("#main%(figid)s");
+var overviewfig = jQuery("#overview%(figid)s");
+
+if ((mainfig.attr('cubicweb:type') != 'prepared-plot') || (overviewfig.attr('cubicweb:type') != 'prepared-plot')){
+
+    var chart_options = {
+        renderTo: 'main%(figid)s',
+        zoomType: 'x'
+    } 
+    
+    var color_values = [
+        '#007a69',
+        '#4572A7', 
+        '#AA4643', 
+        '#89A54E', 
+        '#80699B', 
+        '#3D96AE', 
+        '#DB843D', 
+        '#92A8CD', 
+        '#A47D7C', 
+        '#B5CA92'
+    ] 
+    
+    var title_options = {
+        text: ''
+    } 
+            
+    var subtitle_options = {
+        text: ''
+    } 
+            
+    var xAxis_options = {
+        type: 'datetime',
+        title: {
+            text: null
+        }
+    }
+            
+    var yAxis_options = {
+        title: {
+            text: '.'
+        }
+    }
+            
+    var legend_options = {
+        enabled: false
+    }
+            
+    var plotOptions_options = {
+        line: {
+            animation: false,
+            linewidth: 1,
+            marker: {
+                enabled: false
+            },
+            shadow: false
+        }
+    }
+            
+    var series_data = %(plotdefs)s
+            
+    var series_options = [{
+        type: 'line',
+        data: series_data
+    }]
+             
+    var tooltip_options = {
+        formatter: function() {
+            return Highcharts.dateFormat('%%Y-%%m-%%d %%H:%%M', this.x) + ':<br/>'+
+                + Highcharts.numberFormat(this.y, 2);
+        }
+    }
+            
+    var chart = new Highcharts.Chart({
+        chart: chart_options,
+        colors: color_values,
+        title: title_options,
+        subtitle: subtitle_options,
+        xAxis: xAxis_options,
+        yAxis: yAxis_options,
+        legend: legend_options,
+        plotOptions: plotOptions_options,
+        tooltip: tooltip_options,
+        series: series_options
+    });
+            
+    mainfig.attr('cubicweb:type','prepared-plot');
+    overviewfig.attr('cubicweb:type','prepared-plot');
+}
+
+"""
+
+    def __init__(self, labels, plots):
+        self.labels = labels
+        self.plots = plots # list of list of couples
+
+    def dump_plot(self, plot):
+        plot = [(datetime2ticks(x), y) for x,y in plot]
+        return dumps(plot)
+
+    def _render(self, req, width=900, height=250):
+        if req.ie_browser():
+            req.add_js('excanvas.compiled.js')
+        req.add_js(('highcharts.js', 
+                    'jquery.js'))
+        figid = u'figure%s' % req.varmaker.next()
+        plotdefs = []
+        self.w(u'<div id="main%s" style="width: %spx; height: %spx;"></div>' %
+               (figid, width, height))
+        self.w(u'%nbsp;')
+        for idx, (label, plot) in enumerate(zip(self.labels, self.plots)):
+            plotid = '%s_%s' % (figid, idx)
+            plotdefs.append('%s;' % self.dump_plot(plot))
+
+        req.html_headers.add_onload(self.onload %
+                                    {'plotdefs': '\n'.join(plotdefs),
+                                     'figid': figid,
+                                     },
+                                    jsoncall=req.form.get('jsoncall', False))
