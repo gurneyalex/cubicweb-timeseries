0bb82f05d53a127a8a448f7eb2eef9cd7aa5affb
Alexandre Fayolle <alexandre.fayolle@logilab.fr>
2010-10-07 13:32:37 +0200

    [unittest2] fix deprecation warnings

--- test/test_calendars.py
+++ test/test_calendars.py
@@ -15,12 +15,12 @@ class GasCalendarDateFunctionsTC(TestCase):
     def test_start_of_day_before(self):
         tstamp = datetime(2009, 10, 28, 4)
         start = self.calendar.start_of_day(tstamp)
-        self.assertEquals(start, datetime(2009, 10, 28, 0))
+        self.assertEqual(start, datetime(2009, 10, 28, 0))
 
     def test_next_month_start(self):
         tstamp = datetime(2009, 10, 28, 8)
         start = self.calendar.next_month_start(tstamp)
-        self.assertEquals(start, datetime(2009, 11, 1, 0))
+        self.assertEqual(start, datetime(2009, 11, 1, 0))
 
 if __name__ == '__main__':
     unittest_main()
--- test/test_entities.py
+++ test/test_entities.py
@@ -40,23 +40,23 @@ class TSaccessTC(CubicWebTC):
 
     def test_end_date_daily(self):
         expected_end = self.dailyts.start_date + timedelta(days=10)
-        self.assertEquals(self.dailyts.end_date, expected_end)
+        self.assertEqual(self.dailyts.end_date, expected_end)
 
     def test_end_date_hourly(self):
         expected_end = self.dailyts.start_date + timedelta(hours=10)
-        self.assertEquals(self.hourlyts.end_date, expected_end)
+        self.assertEqual(self.hourlyts.end_date, expected_end)
 
     def test_end_date_weekly(self):
         expected_end = self.weeklyts.start_date + timedelta(days=10*7)
-        self.assertEquals(self.weeklyts.end_date, expected_end)
+        self.assertEqual(self.weeklyts.end_date, expected_end)
 
     def test_end_date_monthly(self):
         expected_end = datetime(2010, 8, 1)
-        self.assertEquals(self.monthlyts.end_date, expected_end)
+        self.assertEqual(self.monthlyts.end_date, expected_end)
 
     def test_end_date_yearly(self):
         expected_end = datetime(2019, 10, 1)
-        self.assertEquals(self.yearlyts.end_date, expected_end)
+        self.assertEqual(self.yearlyts.end_date, expected_end)
 
 
 
@@ -65,28 +65,28 @@ class TSaccessTC(CubicWebTC):
         calendar = self.dailyts.calendar
         granularity = self.dailyts.granularity
         delta = calendar.get_offset(date, granularity) - self.dailyts._start_offset
-        self.assertEquals(delta, 1.5)
+        self.assertEqual(delta, 1.5)
 
     def test_get_by_date_daily(self):
         date = datetime(2009, 10, 2, 12)
-        self.assertEquals(self.dailyts.get_by_date(date), self.dailyts.array[1])
+        self.assertEqual(self.dailyts.get_by_date(date), self.dailyts.array[1])
 
     def test_get_by_date_daily_slice(self):
         date1 = datetime(2009, 10, 2, 12)
         date2 = datetime(2009, 10, 4, 6)
-        self.assertEquals(self.dailyts.get_by_date(slice(date1, date2)).tolist(),
+        self.assertEqual(self.dailyts.get_by_date(slice(date1, date2)).tolist(),
                           self.dailyts.array[1:4].tolist())
 
     def test_get_by_date_daily_slice_end_exact(self):
         date1 = datetime(2009, 10, 2, 12)
         date2 = datetime(2009, 10, 4, 0)
-        self.assertEquals(self.dailyts.get_by_date(slice(date1, date2)).tolist(),
+        self.assertEqual(self.dailyts.get_by_date(slice(date1, date2)).tolist(),
                           self.dailyts.array[1:3].tolist())
 
     def test_get_by_date_daily_slice_below_gran(self):
         date1 = datetime(2009, 10, 2, 12)
         date2 = datetime(2009, 10, 2, 18)
-        self.assertEquals(self.dailyts.get_by_date(slice(date1, date2)).tolist(),
+        self.assertEqual(self.dailyts.get_by_date(slice(date1, date2)).tolist(),
                           self.dailyts.array[1:2].tolist())
 
 
@@ -95,22 +95,22 @@ class TSaccessTC(CubicWebTC):
         calendar = self.monthlyts.calendar
         granularity = self.monthlyts.granularity
         delta = calendar.get_offset(date, granularity) - self.monthlyts._start_offset
-        self.assertFloatAlmostEquals(delta, 1 + 36/(30*24))
+        self.assertAlmostEqual(delta, 1 + 36/(30*24))
 
     def test_get_by_date_monthly(self):
         date = datetime(2009, 11, 2, 12)
-        self.assertEquals(self.monthlyts.get_by_date(date), self.monthlyts.array[1])
+        self.assertEqual(self.monthlyts.get_by_date(date), self.monthlyts.array[1])
 
     def test_get_by_date_monthly_slice(self):
         date1 = datetime(2009, 11, 2, 12)
         date2 = datetime(2010, 1, 4, 6)
-        self.assertEquals(self.monthlyts.get_by_date(slice(date1, date2)).tolist(),
+        self.assertEqual(self.monthlyts.get_by_date(slice(date1, date2)).tolist(),
                           self.monthlyts.array[1:4].tolist())
 
     def test_get_by_date_monthly_slice_below_gran(self):
         date1 = datetime(2009, 11, 2, 12)
         date2 = datetime(2009, 11, 14, 18)
-        self.assertEquals(self.monthlyts.get_by_date(slice(date1, date2)).tolist(),
+        self.assertEqual(self.monthlyts.get_by_date(slice(date1, date2)).tolist(),
                           self.monthlyts.array[1:2].tolist())
 
 
@@ -119,22 +119,22 @@ class TSaccessTC(CubicWebTC):
         calendar = self.yearlyts.calendar
         granularity = self.yearlyts.granularity
         delta = calendar.get_offset(date, granularity) - self.yearlyts._start_offset
-        self.assertFloatAlmostEquals(delta, (365 + 31 + 36/24)/365)
+        self.assertAlmostEqual(delta, (365 + 31 + 36/24)/365)
 
     def test_get_by_date_yearly(self):
         date = datetime(2010, 11, 2, 12)
-        self.assertEquals(self.yearlyts.get_by_date(date), self.yearlyts.array[1])
+        self.assertEqual(self.yearlyts.get_by_date(date), self.yearlyts.array[1])
 
     def test_get_by_date_yearly_slice(self):
         date1 = datetime(2010, 11, 2, 12)
         date2 = datetime(2013, 1, 4, 6)
-        self.assertEquals(self.yearlyts.get_by_date(slice(date1, date2)).tolist(),
+        self.assertEqual(self.yearlyts.get_by_date(slice(date1, date2)).tolist(),
                           self.yearlyts.array[1:4].tolist())
 
     def test_get_by_date_yearly_slice_below_gran(self):
         date1 = datetime(2010, 11, 2, 12)
         date2 = datetime(2011, 1, 14, 18)
-        self.assertEquals(self.yearlyts.get_by_date(slice(date1, date2)).tolist(),
+        self.assertEqual(self.yearlyts.get_by_date(slice(date1, date2)).tolist(),
                           self.yearlyts.array[1:2].tolist())
 
 
@@ -144,23 +144,23 @@ class TSaccessTC(CubicWebTC):
         calendar = self.weeklyts.calendar
         granularity = self.weeklyts.granularity
         delta = calendar.get_offset(date, granularity) - self.weeklyts._start_offset
-        self.assertFloatAlmostEquals(delta, (7 + 2 + 12/24)/7)
+        self.assertAlmostEqual(delta, (7 + 2 + 12/24)/7)
 
     def test_make_relative_index_weekly2(self):
         date = datetime(2009, 10, 19, 0)
         calendar = self.weeklyts.calendar
         granularity = self.weeklyts.granularity
         delta = calendar.get_offset(date, granularity) - self.weeklyts._start_offset
-        self.assertFloatAlmostEquals(delta, 2)
+        self.assertAlmostEqual(delta, 2)
 
     def test_get_by_date_weekly(self):
         date = datetime(2009, 10, 14, 12)
-        self.assertEquals(self.weeklyts.get_by_date(date), self.weeklyts.array[1])
+        self.assertEqual(self.weeklyts.get_by_date(date), self.weeklyts.array[1])
 
     def test_get_by_date_weekly_slice(self):
         date1 = datetime(2009, 10, 14, 12)
         date2 = datetime(2009, 11, 1, 18)
-        self.assertEquals(self.weeklyts.get_by_date(slice(date1, date2)).tolist(),
+        self.assertEqual(self.weeklyts.get_by_date(slice(date1, date2)).tolist(),
                           self.weeklyts.array[1:4].tolist())
 
     def test_get_by_date_weekly_slice_below_gran(self):
@@ -168,7 +168,7 @@ class TSaccessTC(CubicWebTC):
         date2 = datetime(2009, 10, 14, 18)
         self.weeklyts.get_by_date(date1)
         self.weeklyts.get_by_date(date2)
-        self.assertEquals(self.weeklyts.get_by_date(slice(date1, date2)).tolist(),
+        self.assertEqual(self.weeklyts.get_by_date(slice(date1, date2)).tolist(),
                           self.weeklyts.array[1:2].tolist())
 
     def test_aggregated_value_average(self):
@@ -176,21 +176,21 @@ class TSaccessTC(CubicWebTC):
         date2 = datetime(2009, 10, 4, 6)
         _date, result = self.dailyts.aggregated_value([(date1, date2)], 'average')
         expected = (self.dailyts.array[1] + self.dailyts.array[2] + self.dailyts.array[3]) / (3.)
-        self.assertEquals(result, expected)
+        self.assertEqual(result, expected)
 
     def test_aggregated_value_sum(self):
         date1 = datetime(2009, 10, 2, 6)
         date2 = datetime(2009, 10, 4, 6)
         _date, result = self.dailyts.aggregated_value([(date1, date2)], 'sum')
         expected = (.75*self.dailyts.array[1] + 1*self.dailyts.array[2] + .25*self.dailyts.array[3])
-        self.assertEquals(result, expected)
+        self.assertEqual(result, expected)
 
     def test_aggregated_value_last(self):
         date1 = datetime(2009, 10, 2, 6)
         date2 = datetime(2009, 10, 4, 6)
         _date, result = self.dailyts.aggregated_value([(date1, date2)], 'last')
         expected = self.dailyts.array[3]
-        self.assertEquals(result, expected)
+        self.assertEqual(result, expected)
         
     def test_aggregated_value_last_multiple_interval(self):
         interval1 = (datetime(2009, 10, 2, 6), datetime(2009, 10, 4, 6))
@@ -206,7 +206,7 @@ class TSaccessTC(CubicWebTC):
         intervals = [interval1, interval2, interval3]
         _date, result = self.dailyts.aggregated_value(intervals, 'last', use_last_interval=True)
         expected = self.dailyts.array[8]
-        self.assertEquals(result, expected)
+        self.assertEqual(result, expected)
 
     def test_aggregated_value_last_before_start(self):
         date1 = datetime(2009, 9, 2, 6)
@@ -218,28 +218,28 @@ class TSaccessTC(CubicWebTC):
         date2 = datetime(2009, 10, 2, 18)
         _date, result = self.dailyts.aggregated_value([(date1, date2)], 'average')
         expected = (.5*self.dailyts.array[1]) / (.5)
-        self.assertEquals(result, expected)
+        self.assertEqual(result, expected)
 
     def test_aggregated_value_sum_below_gran(self):
         date1 = datetime(2009, 10, 2, 6)
         date2 = datetime(2009, 10, 2, 18)
         _date, result = self.dailyts.aggregated_value([(date1, date2)], 'sum')
         expected = .5*self.dailyts.array[1]
-        self.assertEquals(result, expected)
+        self.assertEqual(result, expected)
 
     def test_aggregated_value_sum_exact_start(self):
         date1 = datetime(2009, 10, 2, 0)
         date2 = datetime(2009, 10, 4, 6)
         _date, result = self.dailyts.aggregated_value([(date1, date2)], 'sum')
         expected = (1*self.dailyts.array[1] + 1*self.dailyts.array[2] + .25*self.dailyts.array[3])
-        self.assertEquals(result, expected)
+        self.assertEqual(result, expected)
 
     def test_aggregated_value_sum_exact_end(self):
         date1 = datetime(2009, 10, 2, 6)
         date2 = datetime(2009, 10, 5, 0)
         _date, result = self.dailyts.aggregated_value([(date1, date2)], 'sum')
         expected = (.75*self.dailyts.array[1] + 1*self.dailyts.array[2] + 1*self.dailyts.array[3])
-        self.assertEquals(result, expected)
+        self.assertEqual(result, expected)
 
 
 
@@ -292,28 +292,28 @@ class ComputeSumAverageTC(CubicWebTC):
         end_date = datetime(2011, 1, 1, 0)
         _date, sum_res = self.yearly_ts.aggregated_value([(start_date, end_date)], 'sum')
         data = self.yearly_ts.array
-        self.assertFloatAlmostEquals(sum_res, data[0] + data[1])
+        self.assertAlmostEqual(sum_res, data[0] + data[1])
 
     def test_yearly_average(self):
         start_date = datetime(2009, 1, 1, 0)
         end_date = datetime(2011, 1, 1, 0)
         _date, average = self.yearly_ts.aggregated_value([(start_date, end_date)], 'average')
         data = self.yearly_ts.array
-        self.assertFloatAlmostEquals(average, (data[0] + data[1])/2)
+        self.assertAlmostEqual(average, (data[0] + data[1])/2)
 
     def test_monthly_sum(self):
         start_date = datetime(2009, 11, 3, 0)
         end_date = datetime(2010, 1, 23, 0)
         _date, sum_res = self.monthly_ts.aggregated_value([(start_date, end_date)], 'sum')
         data = self.monthly_ts.array
-        self.assertFloatAlmostEquals(sum_res, (1-2/30)*data[1] + 1*data[2] + 22/31*data[3])
+        self.assertAlmostEqual(sum_res, (1-2/30)*data[1] + 1*data[2] + 22/31*data[3])
 
     def test_monthly_sum2(self):
         start_date = datetime(2009, 11, 3, 0)
         end_date = datetime(2009, 11, 23, 0)
         _date, sum_res = self.monthly_ts.aggregated_value([(start_date, end_date)], 'sum')
         data = self.monthly_ts.array
-        self.assertFloatAlmostEquals(sum_res, (20/30)*data[1])
+        self.assertAlmostEqual(sum_res, (20/30)*data[1])
 
     def test_monthly_average(self):
         start_date = datetime(2009, 11, 3, 0)
@@ -321,21 +321,21 @@ class ComputeSumAverageTC(CubicWebTC):
         _date, average = self.monthly_ts.aggregated_value([(start_date, end_date)], 'average')
         data = self.monthly_ts.array
         expected = (data[1] + data[2] + data[3]) / (3.)
-        self.assertFloatAlmostEquals(average,  expected)
+        self.assertAlmostEqual(average,  expected)
 
     def test_monthly_average2(self):
         start_date = datetime(2009, 11, 3, 0)
         end_date = datetime(2009, 11, 23, 0)
         _date, average = self.monthly_ts.aggregated_value([(start_date, end_date)], 'average')
         data = self.monthly_ts.array
-        self.assertFloatAlmostEquals(average,  data[1])
+        self.assertAlmostEqual(average,  data[1])
 
     def test_weekly_sum(self):
         start_date = datetime(2009, 10, 10, 0)
         end_date = datetime(2009, 10, 23, 0)
         _date, sum_res = self.weekly_ts.aggregated_value([(start_date, end_date)], 'sum')
         data = self.weekly_ts.array
-        self.assertFloatAlmostEquals(sum_res, 2/7*data[1] + 1*data[2] + 4/7*data[3])
+        self.assertAlmostEqual(sum_res, 2/7*data[1] + 1*data[2] + 4/7*data[3])
 
     def test_weekly_average(self):
         start_date = datetime(2009, 10, 10, 0)
@@ -343,63 +343,63 @@ class ComputeSumAverageTC(CubicWebTC):
         _date, average = self.weekly_ts.aggregated_value([(start_date, end_date)], 'average')
         data = self.weekly_ts.array
         expected = (data[1] + data[2] + data[3]) / (3.)
-        self.assertFloatAlmostEquals(average, expected)
+        self.assertAlmostEqual(average, expected)
 
     def test_daily_sum(self):
         start_date = datetime(2009, 10, 3, 0)
         end_date = datetime(2009, 10, 23, 0)
         _date, sum_res = self.daily_ts.aggregated_value([(start_date, end_date)], 'sum')
         data = self.daily_ts.array
-        self.assertFloatAlmostEquals(sum_res, data[2:22].sum())
+        self.assertAlmostEqual(sum_res, data[2:22].sum())
 
     def test_daily_sum2(self):
         start_date = datetime(2009, 10, 3, 2)
         end_date = datetime(2009, 10, 3, 10)
         _date, sum_res = self.daily_ts.aggregated_value([(start_date, end_date)], 'sum')
         data = self.daily_ts.array
-        self.assertFloatAlmostEquals(sum_res, data[2]*8/24)
+        self.assertAlmostEqual(sum_res, data[2]*8/24)
 
     def test_daily_average(self):
         start_date = datetime(2009, 10, 3, 0)
         end_date = datetime(2009, 10, 23, 0)
         _date, average = self.daily_ts.aggregated_value([(start_date, end_date)], 'average')
         data = self.daily_ts.array
-        self.assertFloatAlmostEquals(average, data[2:22].mean())
+        self.assertAlmostEqual(average, data[2:22].mean())
 
     def test_daily_average2(self):
         start_date = datetime(2009, 10, 3, 2)
         end_date = datetime(2009, 10, 3, 10)
         _date, sum_res = self.daily_ts.aggregated_value([(start_date, end_date)], 'average')
         data = self.daily_ts.array
-        self.assertFloatAlmostEquals(sum_res, data[2])
+        self.assertAlmostEqual(sum_res, data[2])
 
     def test_hourly_sum(self):
         start_date = datetime(2009, 10, 3, 0)
         end_date = datetime(2009, 10, 23, 0)
         _date, sum_res = self.hourly_ts.aggregated_value([(start_date, end_date)], 'sum')
         data = self.hourly_ts.array
-        self.assertFloatAlmostEquals(sum_res, data[2*24:22*24].sum())
+        self.assertAlmostEqual(sum_res, data[2*24:22*24].sum())
 
     def test_hourly_average(self):
         start_date = datetime(2009, 10, 3, 0)
         end_date = datetime(2009, 10, 23, 0)
         _date, average = self.hourly_ts.aggregated_value([(start_date, end_date)], 'average')
         data = self.hourly_ts.array
-        self.assertFloatAlmostEquals(average, data[2*24:22*24].mean())
+        self.assertAlmostEqual(average, data[2*24:22*24].mean())
 
     def test_15min_sum(self):
         start_date = datetime(2009, 10, 3, 0)
         end_date = datetime(2009, 10, 23, 0)
         _date, sum_res = self.quart_ts.aggregated_value([(start_date, end_date)], 'sum')
         data = self.quart_ts.array
-        self.assertFloatAlmostEquals(sum_res, data[2*24*4:22*24*4].sum())
+        self.assertAlmostEqual(sum_res, data[2*24*4:22*24*4].sum())
 
     def test_15min_average(self):
         start_date = datetime(2009, 10, 3, 0)
         end_date = datetime(2009, 10, 23, 0)
         _date, average = self.quart_ts.aggregated_value([(start_date, end_date)], 'average')
         data = self.quart_ts.array
-        self.assertFloatAlmostEquals(average, data[2*24*4:22*24*4].mean())
+        self.assertAlmostEqual(average, data[2*24*4:22*24*4].mean())
 
 
 if __name__ == '__main__':
