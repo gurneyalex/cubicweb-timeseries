f82f48f626a7d96f91a537b8771014bc56c29bb2
Denis Laxalde <denis.laxalde@logilab.fr>
2018-05-25 14:16:41 +0200

    [pkg] Make cube Python3-installable
    We refresh setup.py and __pkginfo__.py using CubicWeb 3.23 skeleton
    which is Python3 compatible and still use the legacy layout.

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -1,5 +1,10 @@
-# pylint: disable-msg=W0622
+# pylint: disable=W0622
 """cubicweb-timeseries application packaging information"""
+
+from os import listdir as _listdir
+from os.path import join, isdir
+from glob import glob
+
 
 modname = 'timeseries'
 distname = 'cubicweb-timeseries'
@@ -12,44 +17,37 @@ description = 'timeseries component for the CubicWeb framework'
 author = 'LOGILAB S.A. (Paris, FRANCE)'
 author_email = 'contact@logilab.fr'
 web = 'http://www.cubicweb.org/project/%s' % distname
-classifiers = [
-    'Environment :: Web Environment',
-    'Framework :: CubicWeb',
-    'Programming Language :: Python',
-    'Programming Language :: JavaScript',
-    ]
-
 
 __depends__ = {
     'cubicweb': '>= 3.16.0',
     'cwtags': None,
     'numpy': None,
 }
-# XXX debian/control tells
-# Depends: python-numpy, python-xlrd, python-xlwt, python-openpyxml
-# Recommends: python-openpyxl
 
-# packaging ###
-
-from os import listdir as _listdir
-from os.path import join, isdir
-from glob import glob
+classifiers = [
+    'Environment :: Web Environment',
+    'Framework :: CubicWeb',
+    'Programming Language :: Python',
+    'Programming Language :: JavaScript',
+]
 
 THIS_CUBE_DIR = join('share', 'cubicweb', 'cubes', modname)
 
+
 def listdir(dirpath):
     return [join(dirpath, fname) for fname in _listdir(dirpath)
-            if fname[0] != '.' and not fname.endswith('.pyc')
-            and not fname.endswith('~')
-            and not isdir(join(dirpath, fname))]
+            if fname[0] != '.' and not fname.endswith('.pyc') and
+            not fname.endswith('~') and
+            not isdir(join(dirpath, fname))]
 
 data_files = [
     # common files
     [THIS_CUBE_DIR, [fname for fname in glob('*.py') if fname != 'setup.py']],
-    ]
+]
 # check for possible extended cube layout
-for dirname in ('entities', 'views', 'sobjects', 'hooks', 'schema', 'data', 'i18n', 'migration', 'wdoc'):
-    if isdir(dirname):
-        data_files.append([join(THIS_CUBE_DIR, dirname), listdir(dirname)])
+for dname in ('entities', 'views', 'sobjects', 'hooks', 'schema', 'data',
+              'wdoc', 'i18n', 'migration'):
+    if isdir(dname):
+        data_files.append([join(THIS_CUBE_DIR, dname), listdir(dname)])
 # Note: here, you'll need to add subdirectories if you want
 # them to be included in the debian package
